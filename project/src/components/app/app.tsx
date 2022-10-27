f198a4c1d999267ce6e239587cf5db5f9a56ecee
vsim1964 <vsim1964@gmail.com>
2022-10-27 11:36:31 +0300

    Первый вариант домашки

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,15 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main/main';
+
+type AppProps = {
+  errorsCount: number;
+}
+
+function App({errorsCount}: AppProps): JSX.Element {
+  return (
+
+    <MainScreen errorsCount={4}/>
+
+  );
 }
 
 export default App;
